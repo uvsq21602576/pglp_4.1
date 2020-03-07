586ce03897d05a4acaa5432849901f8263b4bfe9
Flora Carré <flora.carre@ens.uvsq.fr>
2020-03-07 22:33:49 +0100

    Fin des tests unitaires de telephone.equals.

--- src/test/java/fr/uvsq/uvsq21602576/pglp_4_1/TelephoneTest.java
+++ src/test/java/fr/uvsq/uvsq21602576/pglp_4_1/TelephoneTest.java
@@ -1,5 +1,6 @@
 package fr.uvsq.uvsq21602576.pglp_4_1;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -12,5 +13,20 @@ public class TelephoneTest {
 		Telephone T2 = new Telephone("06", "portable");
 		assertTrue(T.equals(T2));
 	}
+	
+	@Test
+	public void equalsTest2() {
+		Telephone T = new Telephone("065", "portable");
+		Telephone T2 = new Telephone("06", "portable");
+		assertFalse(T.equals(T2));
+	}
+	
+	@Test
+	public void equalsTest3() {
+		Telephone T = new Telephone("06", "porta ble");
+		Telephone T2 = new Telephone("06", "portable");
+		assertFalse(T.equals(T2));
+	}
+
 
 }
