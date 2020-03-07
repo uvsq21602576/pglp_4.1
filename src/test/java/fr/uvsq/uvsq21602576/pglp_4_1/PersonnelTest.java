4627a7eb02ee99b736ad5f9153d22ca69a7eef6b
Flora Carré <flora.carre@ens.uvsq.fr>
2020-03-07 23:11:52 +0100

    Tests Unitaires Personnel terminés.

--- src/test/java/fr/uvsq/uvsq21602576/pglp_4_1/PersonnelTest.java
+++ src/test/java/fr/uvsq/uvsq21602576/pglp_4_1/PersonnelTest.java
@@ -11,12 +11,26 @@ public class PersonnelTest {
 	@Test
 	public void constructeurTest() {
 		Personnel p = new Personnel.Builder("Nom", "Prenom", LocalDate.of(2000,01,05), new Telephone("06...","portable")).build();
-		assertEquals(p.getNom(), "Nom");
-		assertEquals(p.getPrenom(), "Prenom");
-		assertEquals(p.getDateNaissance(), LocalDate.of(2000,01,05));
+		assertEquals("Nom", p.getNom());
+		assertEquals("Prenom", p.getPrenom());
+		assertEquals(LocalDate.of(2000,01,05), p.getDateNaissance());
+		assertEquals("Employé", p.getFonction());
 		
-		assertEquals(p.getNumeros().size(),1);
-		assertEquals(p.getNumeros().get(0), new Telephone("06...", "portable"));
+		assertEquals(1, p.getNumeros().size());
+		assertEquals(new Telephone("06...", "portable"), p.getNumeros().get(0));
+	}
+	
+	@Test
+	public void constructeurTest2() {
+		Personnel p = new Personnel.Builder("Nom", "Prenom", LocalDate.of(2000,01,05), new Telephone("06...","portable")).addNumero(new Telephone("01...", "fixe")).fonction("Directeur").build();
+		assertEquals("Nom", p.getNom());
+		assertEquals("Prenom", p.getPrenom());
+		assertEquals(LocalDate.of(2000,01,05), p.getDateNaissance());
+		assertEquals("Directeur", p.getFonction());
+		
+		assertEquals(2, p.getNumeros().size());
+		assertEquals(new Telephone("01...", "fixe"), p.getNumeros().get(1));
+		assertEquals(new Telephone("06...", "portable"), p.getNumeros().get(0));
 	}
 	
 }
