072f41d70987fde7d77a0039a4f5db02519c2600
Flora Carré <flora.carre@ens.uvsq.fr>
2020-03-07 18:10:45 +0100

    Classe représentant un numéro de tel avec numero et information

---
+++ src/main/java/fr/uvsq/uvsq21602576/pglp_4_1/Telephone.java
@@ -0,0 +1,12 @@
+package fr.uvsq.uvsq21602576.pglp_4_1;
+
+public class Telephone {
+	private String information;
+	private String numero;
+
+	public Telephone(String num, String info) {
+		this.information = info;
+		this.numero = num;
+	}
+
+}
